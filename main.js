cf7152e159784385645a53d7b1e3f24014eec530
Carles Ramon Ruiz <42216936+CarlesRamonRuiz@users.noreply.github.com>
2026-08-11 10:57:58 +0200

    PROGRESS

---
+++ main.js
@@ -0,0 +1,11 @@
+/*const carta = document.getElementById("carta")
+const contacto = document.getElementById("contacto")
+
+carta.onmouseover = function() {
+    fondoc();};
+function fondoc(){
+    carta.style.backgroundImage = "url('imagenes/carta.jpg')".filter = "blur(4px)";
+    carta.style.color = "white";
+}
+carta.onmouseleave()
+*/
